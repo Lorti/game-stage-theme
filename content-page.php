07fa0881367e019045df23271078f6024c850c32
Manuel Wieser <office@manuelwieser.com>
2013-10-26 19:25:49 +0200

    disqus support

--- content-page.php
+++ content-page.php
@@ -11,5 +11,10 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 		<?php the_content(); ?>
+		<?php
+	      // If comments are open or we have at least one comment, load up the comment template
+	      if ( comments_open() || '0' != get_comments_number() )
+	        comments_template();
+	    ?>
 
 </article><!-- #post-<?php the_ID(); ?> -->
